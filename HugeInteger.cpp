51be410b74091046730185e4e23fba327c0d93ee
Kim Jeong Jin <kjjgo35@gmail.com>
2019-10-06 01:19:53 +0900

    HugeInteger.cpp - Modified Error
    C++ How to Program, Chapter 9, Exercises 9.14, p482
    
    Member-functions definitions for class HugeInteger

--- HugeInteger.cpp
+++ HugeInteger.cpp
@@ -1,23 +1,29 @@
-#include "HugeInteger.h"
+// 12191706 ±èÁ¤Áø
+// Chapter 9 - 9.14
+// Member-functions definitions for class HugeInteger.
+#include "HugeInteger.h" // include definition of class HugeInteger from HugeInteger.h
 #include <iostream>
 using namespace std;
 
 //HugeInteger Class Functions
 
-HugeInteger::HugeInteger(){
+// provide a constructor that enables objects of this class to be initialized when they are created.
+HugeInteger::HugeInteger() {
 	for (int i{ 0 }; i < 40; i++) {
 		number[i] = 0;
 	}
 }
 
-HugeInteger::~HugeInteger(){}
+HugeInteger::~HugeInteger() {}
 
-void HugeInteger::input(int num[40]){
+// input 40 one-digit integers for each digit of a 40-digit integer.
+void HugeInteger::input(int num[40]) {
 	for (int i{ 0 }; i < 40; i++) {
 		number[i] = num[i];
 	}
 }
 
+// output 40-digit integer.
 void const HugeInteger::output() {
 	for (int i{ 0 }; i < 40; i++) {
 		cout << number[i];
@@ -25,21 +31,11 @@ void const HugeInteger::output() {
 	cout << "\n";
 }
 
+// distinguish hugeinteger is zero or not.
 bool HugeInteger::isZero() {
-	bool result = false;
-	for (int i{ 0 }; i < 40; i++) {
-		if (number[i] != 0) {
-			result = true;
-			break;
-		}
-    }
-	return result;
-}
-
-bool HugeInteger::isEqualTo(const HugeInteger& other) {
 	bool result = true;
 	for (int i{ 0 }; i < 40; i++) {
-		if (number[i] != other.number[i]) {
+		if (number[i] != 0) {
 			result = false;
 			break;
 		}
@@ -47,13 +43,25 @@ bool HugeInteger::isEqualTo(const HugeInteger& other) {
 	return result;
 }
 
+// distinguish two hugeintegers are equal.
+bool HugeInteger::isEqualTo(const HugeInteger& other) {
+	for (int i{ 0 }; i < 40; i++) {
+		if (number[i] != other.number[i]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+// distinguish two hugeintegers are not equal.
 bool HugeInteger::isNotEqualTo(const HugeInteger& other) {
 	bool result = false;
-	if (isNotEqualTo(other) == false)
+	if (isEqualTo(other) == false)
 		result = true;
 	return result;
 }
 
+// distinguish hugeinteger is greater than other.
 bool HugeInteger::isGreaterThan(const HugeInteger& other) {
 	bool result = false;
 	for (int i{ 0 }; i < 40; i++) {
@@ -67,6 +75,7 @@ bool HugeInteger::isGreaterThan(const HugeInteger& other) {
 	return result;
 }
 
+// distinguish hugeinteger is less than other.
 bool HugeInteger::isLessThan(const HugeInteger& other) {
 	bool result = false;
 	for (int i{ 0 }; i < 40; i++) {
@@ -80,6 +89,7 @@ bool HugeInteger::isLessThan(const HugeInteger& other) {
 	return result;
 }
 
+// distinguish hugeinteger is greater than or equal to other.
 bool HugeInteger::isGreaterThanOrEqualTo(const HugeInteger& other) {
 	bool result = false;
 	if (isGreaterThan(other) == true || isEqualTo(other) == true)
@@ -87,6 +97,7 @@ bool HugeInteger::isGreaterThanOrEqualTo(const HugeInteger& other) {
 	return result;
 }
 
+// distinguish hugeinteger is less than or equal to other.
 bool HugeInteger::isLessThanOrEqualTo(const HugeInteger& other) {
 	bool result = false;
 	if (isLessThan(other) == true || isEqualTo(other) == true)
@@ -94,26 +105,33 @@ bool HugeInteger::isLessThanOrEqualTo(const HugeInteger& other) {
 	return result;
 }
 
+// add two huge integer
 HugeInteger HugeInteger::add(const HugeInteger& other) {
 	HugeInteger result;
+	int save = 0;
 	for (int i{ 39 }; i >= 0; i--) {
-		result.number[i] = number[i] + other.number[i];
+		result.number[i] = number[i] + other.number[i] + save;
 		if (i > 0) {
-			if (result.number[i] >= 10)
-				result.number[i - 1]++;
+			if (result.number[i] >= 10) {
+				result.number[i] -= 10;
+				save = 1;
+			}
 		}
 	}
 	return result;
 }
 
+// subtract two huge integer
 HugeInteger HugeInteger::subtract(const HugeInteger& other) {
 	HugeInteger result;
+	int save = 0;
 	for (int i{ 39 }; i >= 0; i--) {
-		result.number[i] = number[i] - other.number[i];
+		result.number[i] = number[i] - other.number[i] - save;
+		save = 0;
 		if (i > 0) {
 			if (result.number[i] < 0) {
 				result.number[i] += 10;
-				result.number[i - 1]--;
+				save = 1;
 			}
 		}
 	}
