035ee9fb16e40ffe06cf9b2ce33785f1403a1926
Kim Jeong Jin <kjjgo35@gmail.com>
2019-10-07 01:15:42 +0900

    Card.h - Modified Error - Add Commit
    C++ How to Program, Chapter 9, Exercises9.23, p484
    
    Card class definition

--- Card.h
+++ Card.h
@@ -8,20 +8,26 @@
 #ifndef CARD_H
 #define CARD_H
 
+// (a) enum class to represent the faces and suits
 enum class Faces { Ace = 1, Deuce, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King };
 enum class Suits { Spades = 1, Hearts, Diamonds, Clubs };
 
 // Card class definition
 class Card {
 public:
-	Card(Faces f, Suits s);
+	// default constructor
+	explicit Card();
+	// (b) constructor that receives two enumeration constants representing the faces and suits
+	explicit Card(Faces f, Suits s);
 
 	std::string toString(); // face of suit format string
 
 private:
+	// (a)
 	Faces face;
 	Suits suit;
 
+	// (c) Two static arrays of strings representing the faces and suits
 	static std::string s_face[];
 	static std::string s_suit[];
 };
