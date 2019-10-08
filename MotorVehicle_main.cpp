c05ac0f3bdfe233b4b73b7a826dde9b731878747
Kim Jeong Jin <kjjgo35@gmail.com>
2019-10-09 01:42:32 +0900

    MotorVehicle_main.cpp
    C++ How to Program, Chapter 10, Exercises 10.11, p530
    
    driver program to test the functionality of the enhanced MotorVehicle class.
    program enhanced from Chapter 3, Exercises 3.11, p140 - use operator overloading

--- MotorVehicle_main.cpp
+++ MotorVehicle_main.cpp
@@ -12,7 +12,8 @@ int main() {
 
 	// new car has been created and display it's information
 	MotorVehicle newmotor("HD", 2000, 50);
-	cout << "New MotorVehicle has been created.\n";
+	cout << "New MotorVehicle has been created.\n"
+		<< "Display information by displayCarDetails function.";
 	newmotor.displayCarDetails();
 
 	// input new car's information
@@ -36,6 +37,47 @@ int main() {
 	cin >> engine_dur;
 	newmotor.setEngineCapacity(engine_dur);
 
-	// display new car's information
-	newmotor.displayCarDetails();
+	// display new car's information by output operator(<<)
+	cout << "\nDisplay information by output operator." << newmotor;
+
+	// another new car has been created.
+	MotorVehicle anothermotor("BMW", 2010, 1);
+	cout << "Another new MotorVehicle has been created.\n";
+
+	// input anothercar's information
+	cout << "Fill in anothermotor's make : ";
+	cin >> company;
+	anothermotor.setMake(company);
+
+	cout << "Fill in anothermotor's fuelType : ";
+	cin >> fuel_name;
+	anothermotor.setFuelType(fuel_name);
+
+	cout << "Fill in anothermotor's yearOfManufacture : ";
+	cin >> made_year;
+	anothermotor.setYearOfManufacture(made_year);
+
+	cout << "Fill in anothermotor's color : ";
+	cin >> car_color;
+	anothermotor.setColor(car_color);
+
+	cout << "Fill in anothermotor's engineCapacity : ";
+	cin >> engine_dur;
+	anothermotor.setEngineCapacity(engine_dur);
+
+	// display new car's information by output operator(<<)
+	cout << "\nDisplay information by output operator." << newmotor;
+
+	// compare new car and another car and display greater one. (which was manufactured before than other)
+	if (newmotor == anothermotor)
+		cout << "Both motors are same model.\n";
+	else if (newmotor != anothermotor) {
+		cout << "Both motors are not same model.\nDisplay greater motor's information.\n";
+		if (newmotor > anothermotor)
+			cout << "Greater motor is newmotor." << newmotor;
+		else if(anothermotor > newmotor)
+			cout << "Greater motor is anothermotor." << anothermotor;
+		else
+			cout << "Both motors are not same model, but they are both great."
+	}
 }
