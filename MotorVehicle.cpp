9ab982388ec7cedae5db1644dd65f8fc3879cecd
Kim Jeong Jin <kjjgo35@gmail.com>
2019-10-08 21:59:17 +0900

    MotorVehicle.cpp
    C++ How to Program, Chapter 3, Exercises 3.11, p140
    
    Member-functions definitions for class MotorVehicle
    program for prepare Chapter 10, Exercises 10.8, p532

---
+++ MotorVehicle.cpp
@@ -0,0 +1,70 @@
+// 12191706 ±èÁ¤Áø
+// Chapter 10 - 10.8
+// Member-function definitions for class MotorVehicle.
+#include "MotorVehicle.h"
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+// constructor that initializes the three data members
+MotorVehicle::MotorVehicle (string company, int madeyear, int engine_dur)
+: make{ company }, yearOfManufacture{ madeyear }, engineCapacity{ engine_dur } {}
+
+// function to set the Car make
+void MotorVehicle::setMake(string company) {
+	make = company;
+}
+
+// function to retrieve the Car make
+string MotorVehicle::getMake() const {
+	return make;
+}
+
+// function to set the Car fuelType
+void MotorVehicle::setFuelType(string fuel_name) {
+	fuelType = fuel_name;
+}
+
+// function to retrieve the Car fuelType
+string MotorVehicle::getFuelType() const {
+	return fuelType;
+}
+
+// function to set the Car yearOfManufacture
+void MotorVehicle::setYearOfManufacture(int made_year) {
+	yearOfManufacture = made_year;
+}
+
+// function to retrieve the Car yearOfManufacture
+int MotorVehicle::getYearOfManufacture() const {
+	return yearOfManufacture;
+}
+
+// function to set the Car color
+void MotorVehicle::setColor(string car_color) {
+	color = car_color;
+}
+
+// function to retrieve the Car color
+string MotorVehicle::getColor() const {
+	return color;
+}
+
+// function to set the Car engineCapacity
+void MotorVehicle::setEngineCapacity(int engine_dur) {
+	engineCapacity = engine_dur;
+}
+
+// function to retrieve the Car engineCapacity
+int MotorVehicle::getEngineCapacity() const {
+	return engineCapacity;
+}
+
+// displays the five data members in five separate lines in the form "member name: member value"
+void MotorVehicle::displayCarDetails() {
+	cout << "\nmake : " << getMake()
+		<< "\nfuelType : " << getFuelType()
+		<< "\nyearOfManufacture : " << getYearOfManufacture()
+		<< "\ncolor : " << getColor()
+		<< "\nengineCapacity : " << getEngineCapacity() << endl << endl;
+}
