39db1889d8fee2687665a2910e023125c225277e
Kim Jeong Jin <kjjgo35@gmail.com>
2019-10-08 21:57:39 +0900

    MotorVehicle.h
    C++ How to Program, Chapter 3, Exercises 3.11, p140
    
    MotorVehicle class definition
    program for prepare Chapter 10, Exercises 10.8, p532

---
+++ MotorVehicle.h
@@ -0,0 +1,41 @@
+// 12191706 ±èÁ¤Áø
+// Chpater 10 - 10.8
+// MotorVehicle class definition.
+#pragma once
+#include <string>
+
+// prevent multiple inclusions of header
+#ifndef MOTORVEHICLE_H
+#define MOTORVEHICLE_H
+
+class MotorVehicle {
+public:
+	explicit MotorVehicle(std::string ,int ,int); // constructor with three data member
+
+	void setMake(std::string); // set make function
+    std::string getMake() const; // get make function
+
+	void setFuelType(std::string); // set fuelType function
+	std::string getFuelType() const; // get fuelType function
+
+	void setYearOfManufacture(int); // set yearOfManufacture function
+	int getYearOfManufacture() const; // get yearOfManufacture function
+
+	void setColor(std::string); // set color function
+	std::string getColor() const; // get color function
+
+	void setEngineCapacity(int); // set engineCapacity function
+	int getEngineCapacity() const; // get engineCapacity function
+
+	void displayCarDetails(); // display data members function
+
+private:
+	std::string make;
+	std::string fuelType;
+	int yearOfManufacture;
+	std::string color;
+	int engineCapacity;
+};
+
+#endif // !MOTORVEHICLE_H
+
