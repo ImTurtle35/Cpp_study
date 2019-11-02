7a5e8ac9ad49cc9bddc2e45f19ad477cf51213f4
Kim Jeong Jin <kjjgo35@gmail.com>
2019-11-02 22:40:00 +0900

    Exercises_12.9 BasePlusCommissionEmployee.cpp
    C++ How to Program, Chapter 12, Exercises 12.9, p614
    
    Member-Functions definitions for class BasePlusCommissionEmployee

---
+++ BasePlusCommissionEmployee.cpp
@@ -0,0 +1,44 @@
+// Fig. 12.16: BasePlusCommissionEmployee.cpp
+// BasePlusCommissionEmployee member-function definitions.
+#include <iomanip>
+#include <stdexcept>
+#include <sstream>
+#include "BasePlusCommissionEmployee.h"
+using namespace std;
+
+// constructor
+BasePlusCommissionEmployee::BasePlusCommissionEmployee(
+	const string& first, const string& last, const string& ssn,
+	Date& birthdate, double sales, double rate, double salary)
+	: CommissionEmployee(first, last, ssn, birthdate, sales, rate) {
+	setBaseSalary(salary); // validate and store base salary
+}
+
+// set base salary
+void BasePlusCommissionEmployee::setBaseSalary(double salary) {
+	if (salary < 0.0) {
+		throw invalid_argument("Salary must be >= 0.0");
+	}
+
+	baseSalary = salary;
+}
+
+// return base salary
+double BasePlusCommissionEmployee::getBaseSalary() const {
+	return baseSalary;
+}
+
+// calculate earnings;
+// override virtual function earnings in CommissionEmployee
+double BasePlusCommissionEmployee::earnings() const {
+	return  getBaseSalary() + CommissionEmployee::earnings();
+}
+
+// return a string representation of a BasePlusCommissionEmployee
+string BasePlusCommissionEmployee::toString() const {
+	ostringstream output;
+	output << fixed << setprecision(2);
+	output << "base-salaried " << CommissionEmployee::toString()
+		<< "; base salary: " << getBaseSalary();
+	return output.str();
+}
