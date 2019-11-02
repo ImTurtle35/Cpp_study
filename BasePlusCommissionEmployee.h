10ab711a0247d9045169a542564640ee44c8f628
Kim Jeong Jin <kjjgo35@gmail.com>
2019-11-02 22:37:45 +0900

    Exercises_12.9 BasePlusCommissionEmployee.h
    C++ How to Program, Chapter 12, Exercises 12.9, p614
    
    class BasePlusCommissionEmployee definition
    derived from class CommissionEmployee

---
+++ BasePlusCommissionEmployee.h
@@ -0,0 +1,27 @@
+// Fig. 12.15: BasePlusCommissionEmployee.h
+// BasePlusCommissionEmployee class derived from CommissionEmployee.
+#pragma once
+#ifndef BASEPLUS_H
+#define BASEPLUS_H
+
+#include <string> // C++ standard string class
+#include "CommissionEmployee.h" // CommissionEmployee class definition
+
+class BasePlusCommissionEmployee : public CommissionEmployee {
+public:
+	BasePlusCommissionEmployee(const std::string&, const std::string&,
+		const std::string&, Date&, double = 0.0, double = 0.0, double = 0.0);
+	virtual ~BasePlusCommissionEmployee() = default; // virtual destructor
+
+	void setBaseSalary(double); // set base salary
+	double getBaseSalary() const; // return base salary
+
+	// keyword virtual signals intent to override
+	virtual double earnings() const override; // calcualte earnings
+	virtual std::string toString() const override; // string representation
+
+private:
+	double baseSalary; // base salary per week
+};
+#endif // !BASEPLUS_H
+
